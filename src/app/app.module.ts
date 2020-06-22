69be44efd79a3670fd408f2232ed1dbac78bb5ba
Ervin Ademovic <newea1984@gmail.com>
2020-06-22 13:29:01 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RudiNavComponent } from './rudi-nav/rudi-nav.component';
 import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RudiNavComponent,
-    HeroComponent
+    HeroComponent,
+    AboutComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -6,8 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero.component.css']
 })
 export class HeroComponent implements OnInit {
-	
-  constructor() { }
+public heroHeading: string;
+public  heroText: string;
+public heroBtnText : string; 
+public heroBtnUrl:   string ;
+
+  constructor() { 
+    this.heroHeading = "We are your ultimate Winter experience Provider!";
+    this.heroText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, in!";
+    this.heroBtnText = "View more";
+    this.heroBtnUrl = "#"
+  }
 
   ngOnInit(): void {
   }
